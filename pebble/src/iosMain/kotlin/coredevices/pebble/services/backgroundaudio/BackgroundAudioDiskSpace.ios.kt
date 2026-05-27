27ecfb0e5f3922bb44ce7f451b86277eb4647ba2
Roger <roger@betechie.com>
2026-05-27 18:51:23 +0700

    mobile: fix background audio ios storage paths

--- pebble/src/iosMain/kotlin/coredevices/pebble/services/backgroundaudio/BackgroundAudioDiskSpace.ios.kt
+++ pebble/src/iosMain/kotlin/coredevices/pebble/services/backgroundaudio/BackgroundAudioDiskSpace.ios.kt
@@ -1,12 +1,15 @@
 package coredevices.pebble.services.backgroundaudio
 
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.Foundation.NSFileManager
 import platform.Foundation.NSFileSystemFreeSize
+import platform.Foundation.NSNumber
 
+@OptIn(ExperimentalForeignApi::class)
 internal actual fun backgroundAudioAvailableBytes(): Long? {
     val attributes = NSFileManager.defaultManager.attributesOfFileSystemForPath(
         path = backgroundAudioStorageDirectory().toString(),
         error = null,
     ) ?: return null
-    return (attributes[NSFileSystemFreeSize] as? Number)?.toLong()
+    return (attributes[NSFileSystemFreeSize] as? NSNumber)?.longLongValue
 }
--- pebble/src/iosMain/kotlin/coredevices/pebble/services/backgroundaudio/BackgroundAudioPaths.ios.kt
+++ pebble/src/iosMain/kotlin/coredevices/pebble/services/backgroundaudio/BackgroundAudioPaths.ios.kt
@@ -3,6 +3,7 @@ package coredevices.pebble.services.backgroundaudio
 import kotlinx.io.files.Path
 import platform.Foundation.NSDocumentDirectory
 import platform.Foundation.NSFileManager
+import platform.Foundation.NSURL
 import platform.Foundation.NSUserDomainMask
 
 internal actual fun backgroundAudioStorageDirectory(): Path {
@@ -10,6 +11,6 @@ internal actual fun backgroundAudioStorageDirectory(): Path {
         directory = NSDocumentDirectory,
         inDomains = NSUserDomainMask,
     )
-    val base = urls.firstOrNull()?.path ?: error("No documents directory")
+    val base = (urls.firstOrNull() as? NSURL)?.path ?: error("No documents directory")
     return Path(base, "background-audio")
 }
